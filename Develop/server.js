f8d1fd52c38fa93ad9abc49ddc2d2eee1cd0a1bb
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2022-04-15 19:47:30 -0500

    practiced more with server

--- Develop/server.js
+++ Develop/server.js
@@ -17,6 +17,12 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+// this filters the anmails array based on id at end of 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+// app.get is a route; 2 or more app.get = 2 or more routes
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -24,6 +30,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
   });
+// app.get sets a route to retrieve a record; this route searches based on id #
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
 
 app.listen(3005, () => {
     console.log(`API server now on port 3005!`)
